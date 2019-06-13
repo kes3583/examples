707ec30be57410eb8b06e7d0d42c81916ef8b46e
kes3583 <kes3583@gmail.com>
2019-06-13 16:17:21 +0200

    add m

--- fuel/html/js/common.js
+++ fuel/html/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   const h1Pos = $('.title > h1').offset();
   $(window).scroll(function() {
-    if ($(document).scrollTop() > h1Pos.top) {
+    if ($(document).scrollTop() > 50) {
       //console.log(h1Pos.top)
       $("header > .txt").animate({
         opacity: '1'
