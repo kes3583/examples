d39292d7fe3fbb6949830def5f01291d84b65e74
kes3583 <kes3583@gmail.com>
2019-06-13 15:54:11 +0200

    add 3

--- fuel/html/js/common.js
+++ fuel/html/js/common.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   const h1Pos = $('.title > h1').offset();
-
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 87) {
+    if ($(document).scrollTop() > h1Pos.top) {
       //console.log(h1Pos.top)
       $("header > .txt").animate({
         opacity: '1'
+      });
+    } else {
+      $("header > .txt").animate({
+        opacity: '0'
       });
     }
   });
