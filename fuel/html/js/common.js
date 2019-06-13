0de4e19ba0afda53b3c4d078732a2a8516241fa6
kes3583 <kes3583@gmail.com>
2019-06-13 15:51:48 +0200

    add ani2

--- fuel/html/js/common.js
+++ fuel/html/js/common.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   const h1Pos = $('.title > h1').offset();
 
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      console.log(h1Pos.top)
+    if ($(document).scrollTop() > 87) {
+      //console.log(h1Pos.top)
       $("header > .txt").animate({
         opacity: '1'
       });
