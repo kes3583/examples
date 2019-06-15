c7499d087d83e4ee1b98d4fa0e424aad3ca7d4e7
kes3583 <kes3583@gmail.com>
2019-06-15 15:57:07 +0200

    s

--- simple-jquery-modal-popup/dist/script.js
+++ simple-jquery-modal-popup/dist/script.js
@@ -1,7 +1,7 @@
 $(function() {
 // Quick & dirty toggle to demonstrate modal toggle behavior
   $('.modal-toggle').on('click', function(e) {
-    e.preventDefault();
+    // e.preventDefault();
     //$(window).scroll(function() { return false; });
 
     $('.modal').toggleClass('is-visible');
@@ -12,8 +12,8 @@ $(function() {
     // }else{
     //   $('body').addClass('modal-open');
     // }
-    document.ontouchmove = function (e) {
-      e.preventDefault();
-    }
+    // document.ontouchmove = function (e) {
+    //   e.preventDefault();
+    // }
   });
 });
