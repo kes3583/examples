b12bc8f4f9e3d3aa54bde87d2d636530c6ad1945
알마커뮤니케이션(주) 강은심 <kes3583@almacomm.co.kr>
2020-12-07 21:30:43 +0900

    가위바위보

--- zerocho/react/class2/ResponseCheck.js
+++ zerocho/react/class2/ResponseCheck.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
-// function Tries({tryInfo}) { 
-//   return (
-//     <>
-//       <li><b>{tryInfo.try}</b> - {tryInfo.result}</li>
-//     </>
-//   )
-    
-// }
 const ResponseCheck = () => { 
+
+  const [state, setState] = useState('waiting'); //useState는 값이 바뀌면 다시 render가 된다.
+  const [message, setMessage] = useState('Click to start!');
+  const [result, setResult] = useState([]);
+
+  const timeout = useRef(null); //this를 설정해준다. re-render가 안됨.
+  const startTimePoint = useRef(); //useRef 는 current로 접근하라. 
+  const endTimePoint = useRef();
+
+  function onClickScreen(params) {
+    //waiting = aqua, ready = red, now = greenyellow
+
+    if (state === 'waiting') {
+
+      setMessage('초록색이되면 클릭하세요')
+      setState('ready')
+      timeout.current = setTimeout(() => {
+        setState('now')
+        setMessage('Click Now!')
+        startTimePoint.current = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000) //2~3초 뒤
+      
+    } else if(state === 'ready'){
+      clearTimeout(timeout.current)
+      setMessage('Oops! too early! click as soon as green card shows ')
+      setState('waiting')      
+      
+    } else if(state === 'now'){
+      
+      endTimePoint.current = new Date();
+      setState('waiting')     
+      setMessage('click to start')     
+      setResult((prevResult) => {
+        return [...prevResult, endTimePoint.current - startTimePoint.current]
+      })
+    }
+    
+  }
+
+  function onReset(params) {
+    return setResult([])
+    
+  }
+
+  function renderAverage(params) {
+    return result.length === 0 
+    ? null 
+    : <>
+      <div>평균시간: {result.reduce((a,c)=> a + c)/result.length}ms</div> 
+      <button onClick={onReset}>reset</button>
+      </>
+  }
+
   return (
     <>
-      <li><b>response check</b></li>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        <b>{message}</b>
+      </div>
+      {renderAverage()}      
     </>
   )
     
---
+++ zerocho/react/class3/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import RockPaperScissor from './RockPaperScissor';
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <RockPaperScissor />
+      
+     
+    </div>
+  );
+}
+
+export default App;
