692623e2ea06b9483b391d039337a932340307d4
알마커뮤니케이션(주) 강은심 <kes3583@almacomm.co.kr>
2021-02-22 18:36:55 +0900

    minesweeper

--- zerocho/react/class3/my-app/src/pages/MineSweeper.js
+++ zerocho/react/class3/my-app/src/pages/MineSweeper.js
@@ -94,47 +94,92 @@ const reducer = (state, action) => {
     tableData[action.row][action.col] = CODE.OPENED
     console.log('tableData :>> ', tableData[action.row][action.col]);
     console.log('...state.tableData :>> ', tableData[action.row][action.col]);
-    console.log('[...state.tableData[action.row][action.col]]:>> ', ...state.tableData[action.row][action.col]);
+    //console.log('[...state.tableData[action.row][action.col]]:>> ', ...state.tableData[action.row][action.col]);
 
     //모든가로들을 복사한다. 불변성때문에. 
     //[-1,-1,-1,-1,-7]
-    // tableData.forEach((row,i)=>{
-    //   tableData[i] = [...state.tableData[i]]
-    // })
-    console.log('tableData row :>> ', tableData);
-    let aroundCells = []
-    //upper row
-    if (tableData[action.row-1]) {
-      aroundCells = aroundCells.concat(
-        [
-          tableData[action.row-1][action.col-1],
-          tableData[action.row-1][action.col],
-          tableData[action.row-1][action.col+1]  
-        ]
-      )   
-    }  
-    //current cell left, right  
-    aroundCells = aroundCells.concat(
-      [ tableData[action.row][action.col-1], tableData[action.row][action.col+1] ]
-    ) 
+    tableData.forEach((row,i)=>{
+      tableData[i] = [...state.tableData[i]]
+    })
+    
+    //0
+    const checkAroundCells = (row, col) => {
+      if([CODE.OPENED, CODE.FLAG_MINE, CODE.FLAG, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][col])){
+        return;
+      }
+      if(row<0 || row > tableData.length || col < 0 || col > tableData[0].length){
+        return;
+      }
 
-    //lower row
-    if (tableData[action.row+1]) {
-    aroundCells = aroundCells.concat(
-        [
-          tableData[action.row+1][action.col-1],
-          tableData[action.row+1][action.col],
-          tableData[action.row+1][action.col+1]  
-        ]
-      )   
+
+      let aroundCells = []
+      //upper row
+      if (tableData[action.row-1]) {
+        aroundCells = aroundCells.concat(
+          [
+            tableData[action.row-1][action.col-1],
+            tableData[action.row-1][action.col],
+            tableData[action.row-1][action.col+1]  
+          ]
+        )   
+      }  
+      //current cell left, right  
+      aroundCells = aroundCells.concat(
+        [ tableData[action.row][action.col-1], tableData[action.row][action.col+1] ]
+
+      ) 
+
+      //lower row
+      if (tableData[action.row+1]) {
+      aroundCells = aroundCells.concat(
+          [
+            tableData[action.row+1][action.col-1],
+            tableData[action.row+1][action.col],
+            tableData[action.row+1][action.col+1]  
+          ]
+        )   
+      }
+      console.log('aroundCells :>> ', aroundCells);
+      const countMines = aroundCells.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)).length
+      console.log('countMines :>> ', countMines);
+      tableData[action.row][action.col] = countMines
+     
+      if (countMines === 0) {
+       const near = []
+       console.log('row', row)
+       console.log('col', col)
+
+       if(row -1 > -1){ //윗칸이 존재하면
+         near.push([row -1, col -1])
+         near.push([row -1, col])
+         near.push([row -1, col +1])
+       }
+
+       //left, right side
+       near.push([row, col - 1])
+       near.push([row, col + 1])
+
+       //아래칸
+       console.log('tableData.length', tableData.length)
+       if(row + 1 < tableData.length){
+         near.push([row +1, col -1])
+         near.push([row +1, col])
+         near.push([row +1, col +1])
+       }
+       console.log('near[]', near)
+       near.forEach((n)=>{
+        //checkAroundCells(n[0], n[1])
+       })
+      }else{
+        
+      }
     }
-    console.log('aroundCells :>> ', aroundCells);
-    const countMines = aroundCells.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)).length
-    console.log('countMines :>> ', countMines);
-    tableData[action.row][action.col] = countMines
-    if (countMines === 0) {
-      
-    }
+
+    //0이 아닐경우 
+    checkAroundCells(action.row, action.col)
+    console.log('tableData row :>> ', tableData);
+
+
     return {
       ...state, 
       tableData
